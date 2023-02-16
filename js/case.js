98b2e4d065ec3ca90c7f291967ad88111736aca6
mdjewel999 <mdjewel878761@gmail.com>
2023-02-17 01:05:08 +0600

    new commit

--- js/case.js
+++ js/case.js
@@ -1,8 +1,32 @@
-document.getElementById("bt-case-plus").addEventListener("click", function () {
+/* 
+1. add event listener to the case plus button
+2. get the valu inside the case number field (input field)
+3. convert the number to an integer 
+4. calculate the new case number
+5. set the valu to the case number field
+*/
+function updateCaseNumber(isIncrease) {
   const caseNumberField = document.getElementById("case-number-field");
   const caseNumberString = caseNumberField.value;
-  const previousNumber = parseInt(caseNumberString);
-
-  const newCaseNumber = previousNumber + 1;
+  const previouseCaseNumber = parseInt(caseNumberString);
+  let newCaseNumber;
+  if (isIncrease === true) {
+    newCaseNumber = previouseCaseNumber + 1;
+  } else {
+    newCaseNumber = previouseCaseNumber - 1;
+  }
   caseNumberField.value = newCaseNumber;
+  return newCaseNumber;
+}
+document.getElementById("bt-case-plus").addEventListener("click", function () {
+  const newCaseNumber = updateCaseNumber(true);
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
 });
+
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
+    updateCaseNumber(false);
+  });
